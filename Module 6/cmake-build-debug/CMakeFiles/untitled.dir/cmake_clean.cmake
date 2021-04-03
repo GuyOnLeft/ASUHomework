819efa0d7dd6bfbdf8f38a8ae242b91ab7a56456
Jeremy Munson <jeremymunson@Jeremys-MBP-2.attlocal.net>
2021-04-02 21:26:45 -0700

    Initial commit

---
+++ Module 6/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/bmp.c.o"
+  "CMakeFiles/untitled.dir/bmpinfo.c.o"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
